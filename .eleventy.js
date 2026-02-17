1656406f7e15d32749fde4276ec05a5a6f94aa4c
fathomresearch <admin@fathomresearch.ai>
2026-02-17 15:38:30 -0600

    add CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/CNAME");
 
   eleventyConfig.addCollection("caseStudy", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/case-studies/*.md");
