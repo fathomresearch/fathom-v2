ae3cc3ec0f7162800443d9cbeb2599db80d458f4
fathomresearch <admin@fathomresearch.ai>
2026-02-17 16:05:35 -0600

    fix asset paths for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -2,20 +2,21 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
-    eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
 
   eleventyConfig.addCollection("caseStudy", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/case-studies/*.md");
   });
 
   return {
-  dir: {
-    input: "src",
-    output: "public",
-    includes: "_includes",
-    data: "_data"
-  },
-  templateFormats: ["njk", "md", "html"],
-  htmlTemplateEngine: "njk",
-  markdownTemplateEngine: "njk"
+    dir: {
+      input: "src",
+      output: "public",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["njk", "md", "html"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
 };
